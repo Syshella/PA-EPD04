e2dc65d888325238a65a62cb4fdd0fd6aadfe076
Pablo Casas Gómez <pcasgom@upo.es>
2022-11-05 20:30:53 +0100

    Problema 2 Fran y Pablo

---
+++ P2/Problema2FrontEnd.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>FrontEnd Problema2</title>
+    </head>
+    <body>
+        
+        <form action="./Problema2Back.php" method="POST">
+            <textarea id="area" name="frase" rows="5" cols="80"></textarea>
+            <br>
+            <textarea id="area" name="anonimizar" rows="5" cols="80"></textarea>
+            <br>
+            <button type="submit" name="envio">Enviar</button>
+        </form>
+        
+        
+    </body>
+</html>
